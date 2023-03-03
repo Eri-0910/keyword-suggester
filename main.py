a1111b1ef570ef3bf0d6935bf4236832930beef2
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2023-03-03 11:56:59 +0900

    check katakana

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def keyword_suggester():
     keyword_list = keyword_list_load()
+    katakana_list = katakana_list_load()
     keyword_count = {}
+    katakana_count = {}
     s = request.json['sentence']
+    s_katakana = ""
 
     # 形態素解析追加
     wakati = MeCab.Tagger("-Owakati")
     node = wakati.parseToNode(s)
     while node:
+        features = node.feature.split(",")
+        if len(features) > 6:
+            s_katakana += node.feature.split(",")[6]
         if node.feature.split(",")[0] == "動詞":
             # 動詞の時のみ原型も確認
             s += "," + node.feature.split(",")[7]
@@ -24,12 +30,34 @@ def keyword_suggester():
         count =  s.count(keyword)
         if count > 0:
             keyword_count[keyword] = count
-    return keyword_count
+
+    # 読みでチェック
+    for (i, katakana) in enumerate(katakana_list):
+        if katakana == "":
+            continue
+
+        # カタカナに対応するような元のキーワードを選ぶ
+        keyword = keyword_list[i]
+
+        # 探す
+        count =  s_katakana.count(katakana)
+        if count > 0:
+            katakana_count[keyword] = count
+
+    # 結果のコンバイン
+    res = {**katakana_count, **keyword_count}
+
+    return res
+
+def katakana_list_load():
+    return list_load('keywords_katakana.txt')
 
 def keyword_list_load():
+    return list_load('keywords.txt')
+
+def list_load(file):
     list = []
-    with open('keywords.txt', "r") as f:
+    with open(file, "r") as f:
         list = [k.strip() for k in f.readlines()]
     f.close()
     return list
-
