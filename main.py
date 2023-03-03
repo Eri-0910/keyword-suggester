cf6fefc3d12efa38fed71161034a308a06baaaa2
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2023-03-03 16:30:09 +0900

    suggest API add

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import request
 import MeCab
+from transformers import AlbertTokenizer, AlbertForPreTraining
+import torch
 
 app = Flask(__name__)
+
+tokenizer = AlbertTokenizer.from_pretrained('ALINEAR/albert-japanese-v2')
+model = AlbertForPreTraining.from_pretrained('ALINEAR/albert-japanese-v2', output_hidden_states = True,)
 
 @app.route("/", methods=['POST'])
 def keyword_suggester():
@@ -48,6 +53,29 @@ def keyword_suggester():
     res = {**katakana_count, **keyword_count}
 
     return res
+
+@app.route("/suggest", methods=["POST"])
+def suggest():
+    s = request.json['sentence']
+    s_embedding = embedding_avg(s)
+
+    keyword_list = keyword_list_load()
+    keyword_count = {}
+
+    for keyword in keyword_list:
+        key_embedding = embedding_avg(keyword)
+
+        keyword_count[keyword] = torch.cosine_similarity(s_embedding, key_embedding, dim = 0).item()
+
+    sorted_count_list = sorted(keyword_count.items(), key = lambda keyword_count : keyword_count[1])
+
+    return dict(sorted_count_list[-20:])
+
+def embedding_avg(s):
+    input_ids = torch.tensor(tokenizer.encode(s, add_special_tokens=True)).unsqueeze(0)  # Batch size 1
+    outputs = model(input_ids)
+
+    return torch.mean(outputs[0][0], dim=0)
 
 def katakana_list_load():
     return list_load('keywords_katakana.txt')
